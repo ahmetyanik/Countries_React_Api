624d461851eb464a92de894268848a3c59b5593d
admin <ahmetyank4242@gmail.com>
2021-06-05 13:53:53 +0200

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
+function Main(){
+
+    const [countries,setCountries]=useState([]);
+
+
+
+    function getCountries(){
+
+        axios.get("https://restcountries.eu/rest/v2/all").then(function(info){
+            setCountries(info.data);
+        })
+    };
+
+
+    useEffect(getCountries,[]);
+
+    console.log(countries);
+
+
+ 
+
+
+
+
+    return(
+        <div>
+            Main
+        </div>
+    )
+};
+
+
+export default Main;
